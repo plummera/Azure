c2359896ff046269e7ab5e0da710d7f576c349c0
Anthony Plummer <aplummer@psda.com>
2017-10-10 18:38:44 -0400

    Ginormous foul-up in the main Jumbotron! - WOW!

--- link/static/app/js/link.js
+++ link/static/app/js/link.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
-  $('div').scrollspy({ target: '#console' });
+  $('div').scrollspy({ target: '#screen' });
+  $('body').scrollspy({ target: '#navbar-example' })
 
   $('[data-spy="scroll"]').each(function () {
     var $spy = $(this).scrollspy('refresh')
