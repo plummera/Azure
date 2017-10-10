22764870f973a49c71055f7f9eab51b880327275
Anthony Plummer <aplummer@psda.com>
2017-10-10 18:45:54 -0400

    grammar was not a foresight with this app, now it is.

--- link/static/app/js/link.js
+++ link/static/app/js/link.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
   $('div').scrollspy({ target: '#screen' });
-  $('body').scrollspy({ target: '#navbar-example' })
+  $('body').scrollspy({ target: '#navbar-example' })Ch0c0late!
+
 
   $('[data-spy="scroll"]').each(function () {
     var $spy = $(this).scrollspy('refresh')
