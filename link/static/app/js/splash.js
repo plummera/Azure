0b07a73862c04ee1f9335870da3a6e14ee86b876
Anthony Plummer <aplummer@psda.com>
2017-10-29 10:04:19 -0400

    Updated to add donations service

--- link/static/app/js/splash.js
+++ link/static/app/js/splash.js
@@ -1,3 +1,53 @@
 $(document).ready(function() {
   console.log("Hello!");
+
+  function donateStream() {
+
+    var doners = [
+      "bitcoin",
+      "ethereum",
+    ];
+
+    function seriously() {
+
+          var txt = [
+            '<p>Help us make the internet a safe environment for everybody. Send  <img id="btc" src="//anthonyplummer.elementfx.com/wp-content/uploads/2017/10/btc-logo-1.png"> to: <a href="#"><span>1EXth6hM97rZAawuGo1CN9Qqc6CmYqgYSG</span></a></p>',
+            '<p>No <img id="btc" src="//anthonyplummer.elementfx.com/wp-content/uploads/2017/10/btc-logo-1.png">? Send <img id="ethereum" src="//anthonyplummer.elementfx.com/wp-content/uploads/2017/10/ethereum.png"> to : <span>1EXth6hM97rZAawuGo1CN9Qqc6CmYqgYSG</span></p>',
+          ];
+
+          function change() {
+            setInterval("$('.btcDonate').css('opacity', '1')", 1000);
+            $('.btcDonate').html(txt[1]);
+          }
+
+          txt1 = $('.btcDonate').html();
+          if (txt1.match(txt[0])) {
+            setInterval("$('.btcDonate').css('opacity', '0')", 1000);
+            setInterval(change, 4000);
+          } else {
+            $('.btcDonate').append(txt[0]);
+          };
+
+          $('.btcDonate').css('opacity', '1');
+
+    };
+
+    setTimeout(seriously, 0);
+    $('.btcDonate').css('opacity', '0');
+    setTimeout(seriously, 5000);
+    $('.btcDonate').css('opacity', '0');
+    setTimeout(seriously, 5000);
+    $('.btcDonate').css('opacity', '0');
+    setTimeout(seriously, 5000);
+
+
+  };
+
+  function fadeInBTC() {
+    $('.btcDonate').css('opacity', '1');
+    donateStream();
+  };
+
+  setTimeout(fadeInBTC, 4773);
+
 });
